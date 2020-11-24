b7e306bf01ce5f1b71a35d72c02c47900161ea58
Satwik Ram <satwikram29@gmail.com>
2020-11-24 21:36:16 +0530

    Fix Needed

--- deployment/my_app/views.py
+++ deployment/my_app/views.py
@@ -1,10 +1,13 @@
+from PIL.Image import Image
+from django.contrib import messages
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
-
+import os
 
 # Create your views here.
 def home(request):
@@ -17,12 +20,24 @@ def predict(request):
             image = request.FILES['scene']
             print("image.name")
             folder = 'media/images/'
+            print("The Type is:",type(image))
+
+            try:
+                im = Image.open(image.name)
+                print("Image Read")
+                if im.format not in ('BMP', 'PNG', 'JPEG'):
+                    raise ValidationError("Unsupport image type. Please upload bmp, png or jpeg")
+                    messages.info(request, "Please upload an Image Type of Png or Jpg")
+
+            except Exception as e:
+                print("The Error is:",e)
+
             filename = str(image.name)
 
             media_path = folder+filename
             print(media_path)
 
-            prediction = '2'
+            prediction = 2
 
             print("worked")
             data = {"image_path": media_path, "prediction": prediction}
